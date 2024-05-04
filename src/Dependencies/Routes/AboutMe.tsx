1ec87f002ce1c85d01ccf4450461de5a2bcba3cc
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-05-04 17:08:23 +1000

    Updates, Summary changes, Profile picture switched to NASA Picture of the day

--- src/Dependencies/Routes/AboutMe.tsx
+++ src/Dependencies/Routes/AboutMe.tsx
@@ -1,30 +1,90 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+
+
+interface NASAData
+{
+    date: string;
+    explanation: string;
+    hdurl: string; 
+    media_type: string;
+    service_version: string;
+    title: string;
+    url: string; 
+}
 
 export default function AboutMe()
 {
+    const [imageData, setImageData] = useState({
+        date: new Date().toISOString(),
+        explanation: "Failed to collect image of the day",
+        hdurl: "", 
+        media_type: 'image',
+        service_version: '',
+        title: 'Default',
+        url: "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1714003200&semt=sph", 
+    })
+
+    const NASAADDRESS = process.env.REACT_APP_NASA_API_KEY;
+    class handleImage
+    {
+        static getPictureOfTheDay = async () => 
+        {
+            try {
+
+                const cachedData = localStorage.getItem("nasaData");
+                if (cachedData) 
+                {
+                    setImageData(JSON.parse(cachedData))
+                    return;
+                }
+                const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${NASAADDRESS}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                const data: NASAData = await response.json();
+                if (data)
+                {
+                    setImageData(data)
+                    localStorage.setItem("nasaData", JSON.stringify(data))                    
+                }
+                else 
+                {
+                    console.error('Media type is not an image');
+                }
+            } catch (error) {
+                console.error('Error fetching data');
+            }
+        }
+    }
+    useEffect(() => {
+        handleImage.getPictureOfTheDay();
+    }, [])
+
+
+
     return (
-        <div className="bg-LIGHT md:h-[95vh] h-[200vh] w-full">
+        <div className="bg-LIGHT md:h-full w-full">
             <div className="bg-WHITE h-full w-[90%] flex flex-col m-auto shadow-lg">
-                <div className="flex md:flex-row flex-col mt-5 pb-2 rounded">
-                    <div className="w-full md:w-[50%] md:h-[50%] h-full mt-auto mb-auto flex flex-col items-center">
+                <div className="flex flex-col mt-5 pb-2 rounded ">
+                    <div className="w-full md:w-[50%] md:h-[50%] h-full m-auto flex flex-col items-center mb-5">
                         <h1 className="flex m-auto w-[80%] text-center justify-center text-HIGHLIGHT font-serif text-[3rem]">
                             About me
                         </h1>
-                        <p className="flex m-auto w-[80%] text-center md:mb-0 mb-10">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ducimus temporibus vero repudiandae voluptate odit consequuntur sequi, tenetur nemo autem fugiat, quibusdam necessitatibus recusandae in natus vitae commodi qui ex!
+                        <p className="flex m-auto md:w-full w-[80%] text-center md:mb-0 mb-10">
+                        An intermediate Web Developer with experience and skills that are reflected in my use of the following; React, JS, TS, C#, Use of libraries such as Tailwind and framer-motion, Front and Back-end design with SQL Database integration, Stakeholder communications, API Writing, Ubuntu Linux DB Hosting and more according to my persistent personal development.
                         </p>
                     </div>
-                    <div className="md:w-[50%] w-[80%] flex justify-center m-auto">
-                        <img src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.1224184972.1714003200&semt=sph"
-                            alt="Profile Picture"
-                            className="rounded-full ring shadow-lg"
+                    <div className="md:w-[80%] w-[90%] flex justify-center m-auto">
+                        <img src={`${imageData.url}`}
+                            alt={`${imageData.title}`}
+                            className="rounded-lg shadow-lg"
                         />
                     </div>
                 </div>
                 <h1 className="flex m-auto mb-5 w-[80%] text-center justify-center text-HIGHLIGHT font-serif text-[3rem] border-b">
                     My Experiences
                 </h1>
-                <div className=" h-full justify-evenly flex flex-col">
+                <div className=" md:h-[50vh] h-[100vh] justify-evenly flex flex-col">
                     <ul className="flex md:flex-row flex-col w-full h-full justify-evenly">
                         <li className="shadow-lg md:h-[80%] h-[30%] w-full md:w-[30%] shadow-inner-lg">
                             <h1 className="bg-DARK text-LIGHT rounded p-2 text-center">
@@ -41,7 +101,10 @@ export default function AboutMe()
                                     Responsible API design
                                 </li>
                                 <li>
-                                    Organised OOP
+                                    Class based Programming
+                                </li>
+                                <li>
+                                    Api use, see Nasa image of the day above
                                 </li>
                             </ul>
                         </li>
@@ -51,16 +114,16 @@ export default function AboutMe()
                             </h1>
                             <ul className="flex flex-col h-[25vh] text-center justify-evenly w-[80%] m-auto">
                                 <li>
-                                    Open to criticism and feedback
+                                    Open attitude towards feedback and reception of work
                                 </li>
                                 <li>
                                     Discussion and open Collaboration with developers and clientelle
                                 </li>
                                 <li>
-                                    Learning fresh Technology
+                                    Persistent attitude towards learning
                                 </li>
                                 <li>
-                                    Research and developing skills
+                                    Research and developing previously developed skills
                                 </li>
                             </ul>
                         </li>
@@ -79,7 +142,7 @@ export default function AboutMe()
                                     Practiced knowledge of CSS styling using Tailwind CSS
                                 </li>
                                 <li>
-                                    Experienced with CyberSecurity standards such as Sanitizing, Encryption and LOA
+                                    Experienced with CyberSecurity standards such as Sanitizing, Encryption, LOA And social engineering
                                 </li>
                             </ul>
                         </li>
@@ -88,4 +151,9 @@ export default function AboutMe()
             </div>
         </ div>
     )
+}
+
+function get(arg0: string)
+{
+    throw new Error("Function not implemented.")
 }
